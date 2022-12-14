86e7a65f312cf0bc0256807f5a4a1b8d2ea7f4e2
davidkharbin <david_k_harbin@hotmail.com>
2022-12-14 11:48:07 -0700

    moving workstation (sync)

--- 2022/day_05/stacks.ts
+++ 2022/day_05/stacks.ts
@@ -14,6 +14,7 @@ interface Stack {
   [n: string]: string[];
 }
 
+// Part one (crane moves one crate at a time)
 const moveBoxes = (arr: string[]) => {
   const stacks: Stack = {
     1: ["F", "C", "P", "G", "Q", "R"],
@@ -37,6 +38,33 @@ const moveBoxes = (arr: string[]) => {
       stacks[toStack].push(item);
       count--;
     }
+  });
+
+  return stacks;
+};
+
+// Part two: crate moves multiple crates at once
+const moveMultipleBoxes = (arr: string[]) => {
+  const stacks: Stack = {
+    1: ["F", "C", "P", "G", "Q", "R"],
+    2: ["W", "T", "C", "P"],
+    3: ["B", "H", "P", "M", "C"],
+    4: ["L", "T", "Q", "S", "M", "P", "R"],
+    5: ["P", "H", "J", "Z", "V", "G", "N"],
+    6: ["D", "P", "J"],
+    7: ["L", "G", "P", "Z", "F", "J", "T", "R"],
+    8: ["N", "L", "H", "C", "F", "P", "T", "J"],
+    9: ["G", "V", "Z", "Q", "H", "T", "C", "W"],
+  };
+
+  arr.forEach((procedure: string) => {
+    let count: number = Number(procedure[0]); // this many crates
+    let fromStack: string = procedure[1]; // from this stack
+    let toStack: string = procedure[2]; // to this stack:
+
+    let crates = stacks[fromStack].slice(count);
+    stacks[fromStack] = stacks[fromStack].slice(0, count);
+    stacks[toStack] = [...toStack, ...crates];
   });
 
   return stacks;
