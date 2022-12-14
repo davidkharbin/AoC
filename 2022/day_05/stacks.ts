43f22927348c7dadef4031f0e2beab69ca4e0963
davidkharbin <david_k_harbin@hotmail.com>
2022-12-14 15:46:09 -0700

    chore: cleanup and refactored

--- 2022/day_05/stacks.ts
+++ 2022/day_05/stacks.ts
@@ -1,10 +1,10 @@
 export {};
 const fs = require("fs");
 const path = "./input.txt";
-// const path = "./inputShort.txt";
 
 const input: string[] = fs.readFileSync(path, "utf8").split("\n");
 
+// Steps for moving crates. For example: "move n crates from stack a to stack b"
 const procedures: number[][] = input.map((line) => {
   return line
     .split(" ")
@@ -12,25 +12,27 @@ const procedures: number[][] = input.map((line) => {
     .map((str) => Number(str));
 });
 
-interface Stack {
+interface Container {
   [n: string]: string[];
 }
 
-// Part one (crane moves one crate at a time)
-const moveBoxes = (arr: number[][]) => {
-  const stacks: Stack = {
-    1: ["F", "C", "P", "G", "Q", "R"],
-    2: ["W", "T", "C", "P"],
-    3: ["B", "H", "P", "M", "C"],
-    4: ["L", "T", "Q", "S", "M", "P", "R"],
-    5: ["P", "H", "J", "Z", "V", "G", "N"],
-    6: ["D", "P", "J"],
-    7: ["L", "G", "P", "Z", "F", "J", "T", "R"],
-    8: ["N", "L", "H", "C", "F", "P", "T", "J"],
-    9: ["G", "V", "Z", "Q", "H", "T", "C", "W"],
-  };
+const containers: Container = {
+  1: ["F", "C", "P", "G", "Q", "R"],
+  2: ["W", "T", "C", "P"],
+  3: ["B", "H", "P", "M", "C"],
+  4: ["L", "T", "Q", "S", "M", "P", "R"],
+  5: ["P", "H", "J", "Z", "V", "G", "N"],
+  6: ["D", "P", "J"],
+  7: ["L", "G", "P", "Z", "F", "J", "T", "R"],
+  8: ["N", "L", "H", "C", "F", "P", "T", "J"],
+  9: ["G", "V", "Z", "Q", "H", "T", "C", "W"],
+};
 
-  arr.forEach((procedure: number[]) => {
+// Part one (crane moves one crate at a time)
+const moveOneAtATime = (instructions: number[][]) => {
+  const stacks = JSON.parse(JSON.stringify(containers));
+
+  instructions.forEach((procedure: number[]) => {
     let count: number = Number(procedure[0]); // this many times
     let fromStack: number = procedure[1]; // from this stack
     let toStack = procedure[2]; // to this stack:
@@ -41,52 +43,33 @@ const moveBoxes = (arr: number[][]) => {
       count--;
     }
   });
-
   return stacks;
 };
 
 // Part two: crate moves multiple crates at once
-const moveMultipleBoxes = (arr: number[][]) => {
-  const stacks: Stack = {
-    1: ["F", "C", "P", "G", "Q", "R"],
-    2: ["W", "T", "C", "P"],
-    3: ["B", "H", "P", "M", "C"],
-    4: ["L", "T", "Q", "S", "M", "P", "R"],
-    5: ["P", "H", "J", "Z", "V", "G", "N"],
-    6: ["D", "P", "J"],
-    7: ["L", "G", "P", "Z", "F", "J", "T", "R"],
-    8: ["N", "L", "H", "C", "F", "P", "T", "J"],
-    9: ["G", "V", "Z", "Q", "H", "T", "C", "W"],
-  };
-  // const stacks: Stack = {
-  //   1: ["z", "n"],
-  //   2: ["m", "c", "d"],
-  //   3: ["p"],
-  // };
+const moveMultipleBoxes = (instructions: number[][]) => {
+  const stacks = JSON.parse(JSON.stringify(containers));
 
-  arr.forEach((procedure: number[]) => {
+  instructions.forEach((procedure: number[]) => {
     let count: number = procedure[0]; // this many crates
     let fromStack: string = String(procedure[1]); // from this stack
     let toStack: string = String(procedure[2]); // to this stack:
-    console.log("before", stacks);
-    console.log(count, stacks[fromStack]);
-    console.log(count, stacks[toStack]);
-
     let load = stacks[fromStack].slice(-count);
-    console.log("load", load);
+
     while (count) {
       stacks[fromStack].pop();
       count--;
     }
 
     stacks[toStack] = [...stacks[toStack], ...load];
-    console.log("after", stacks);
   });
+
   return stacks;
 };
 
-const getTopCrates = (finishedStack: Stack) => {
+const getTopCrates = (finishedStack: Container) => {
   let topCrates: string = "";
+
   for (let i = 1; i <= 9; i++) {
     let lastItem = finishedStack[i][finishedStack[i].length - 1];
     topCrates += lastItem;
@@ -94,7 +77,7 @@ const getTopCrates = (finishedStack: Stack) => {
   return topCrates;
 };
 
-const stacks9000 = moveBoxes(procedures);
+const stacks9000 = moveOneAtATime(procedures);
 const stacks9001 = moveMultipleBoxes(procedures);
 
 console.log(getTopCrates(stacks9000));
