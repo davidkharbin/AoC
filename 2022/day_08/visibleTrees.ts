088a202031718d1e0b7aabbd1f56ea0dd55d894b
davidkharbin <david_k_harbin@hotmail.com>
2023-01-03 14:33:50 -0600

    refactor: refactors part one

--- 2022/day_08/visibleTrees.ts
+++ 2022/day_08/visibleTrees.ts
@@ -1,34 +1,35 @@
-import { trees } from "./input";
+import { input } from "./input";
 
-let total: number = (trees.length - 2) * 2 + trees[0].length * 2;
+function getVisibleTrees(trees: string[] = input) {
+  let total: number = (trees.length - 2) * 2 + trees[0].length * 2;
 
-for (let row = 1; row < trees.length - 1; row++) {
-  for (let column = 1; column < trees[row].length - 1; column++) {
-    if (testLeft(column, trees[row])) {
-      total++;
-      continue;
-    }
+  for (let row = 1; row < trees.length - 1; row++) {
+    for (let column = 1; column < trees[row].length - 1; column++) {
+      if (testLeft(trees, column, trees[row])) {
+        total++;
+        continue;
+      }
 
-    if (testRight(column, trees[row])) {
-      total++;
-      continue;
-    }
+      if (testRight(trees, column, trees[row])) {
+        total++;
+        continue;
+      }
 
-    if (testUp(row, column, trees[row][column])) {
-      total++;
-      continue;
-    }
+      if (testUp(trees, row, column, trees[row][column])) {
+        total++;
+        continue;
+      }
 
-    if (testDown(row, column, trees[row][column])) {
-      total++;
-      continue;
+      if (testDown(trees, row, column, trees[row][column])) {
+        total++;
+        continue;
+      }
     }
   }
+  return total;
 }
 
-console.log(total);
-
-function testUp(row: number, column: number, sNum: string) {
+function testUp(trees: string[], row: number, column: number, sNum: string) {
   let up: number = row - 1;
   while (up >= 0) {
     if (trees[up][column] >= sNum) {
@@ -40,7 +41,7 @@ function testUp(row: number, column: number, sNum: string) {
   return true;
 }
 
-function testDown(row: number, column: number, sNum: string) {
+function testDown(trees: string[], row: number, column: number, sNum: string) {
   let down: number = row + 1;
   while (down < trees.length) {
     if (trees[down][column] >= sNum) {
@@ -52,7 +53,7 @@ function testDown(row: number, column: number, sNum: string) {
   return true;
 }
 
-function testLeft(column: number, s: string) {
+function testLeft(trees: string[], column: number, s: string) {
   let l = column - 1;
   while (l >= 0) {
     if (s[l] >= s[column]) {
@@ -64,7 +65,7 @@ function testLeft(column: number, s: string) {
   return true;
 }
 
-function testRight(column: number, s: string) {
+function testRight(trees: string[], column: number, s: string) {
   let r = column + 1;
   while (r < s.length) {
     if (s[r] >= s[column]) {
@@ -75,3 +76,5 @@ function testRight(column: number, s: string) {
 
   return true;
 }
+
+console.log(getVisibleTrees(input));
