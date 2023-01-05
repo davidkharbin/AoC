0d319defc0216f49363ed583e120adcc224bd050
davidkharbin <david_k_harbin@hotmail.com>
2023-01-05 11:06:58 -0600

    chore: completes day 08

--- 2022/day_08/visibleTrees.ts
+++ 2022/day_08/visibleTrees.ts
@@ -1,5 +1,12 @@
 import { input } from "./input";
 
+// const input: string[] = [
+//   "30373",
+//   "25512", 
+//   "65332",
+//   "33549",
+//   "35390"];
+
 function getVisibleTrees(trees: string[] = input) {
   let total: number = (trees.length - 2) * 2 + trees[0].length * 2;
 
@@ -29,6 +36,51 @@ function getVisibleTrees(trees: string[] = input) {
   return total;
 }
 
+function getScenicScore(trees: string[] = input) {
+  let highest = 0;
+
+  for (let row = 0; row < trees.length; row++) {
+    for (let col = 0; col < trees[row].length; col++) {
+      let up: number, down: number, left: number, right: number;
+      let strNum: string = trees[row][col];
+
+      up = getUpScene(trees, row, col, strNum);
+      down = getDownScene(trees, row, col, strNum);
+      left = getLeftScene(trees, row, col, strNum);
+      right = getRightScene(trees, row, col, strNum);
+      console.log('row: ', row, '\n', 'col: ', col)
+      console.log('strNum: ', strNum, up, down, left, right);
+      let total: number = up * down * left * right;
+      if (total > highest) highest = total;
+    }
+  }
+  return highest;
+}
+
+function getUpScene(
+  trees: string[],
+  row: number,
+  col: number,
+  current: string
+) {
+  let count: number = 0;
+  let up: number = row - 1;
+  while (up >= 0) {
+    let next: string = trees[up][col];
+    if (next < current) {
+      count++;
+    }
+
+    if (next >= current) {
+      count++;
+      return count;
+    }
+    up--;
+  }
+
+  return count || 1;
+}
+
 function testUp(trees: string[], row: number, column: number, sNum: string) {
   let up: number = row - 1;
   while (up >= 0) {
@@ -39,6 +91,32 @@ function testUp(trees: string[], row: number, column: number, sNum: string) {
   }
 
   return true;
+}
+
+
+function getDownScene(
+  trees: string[],
+  row: number,
+  col: number,
+  current: string
+) {
+  let count: number = 0;
+  let down: number = row + 1;
+  while (down < trees.length) {
+    let next: string = trees[down][col];
+    if (next < current) {
+      count++;
+    }
+
+    if (next >= current) {
+      count++;
+      return count;
+    }
+
+    down++;
+  }
+
+  return count || 1;
 }
 
 function testDown(trees: string[], row: number, column: number, sNum: string) {
@@ -53,8 +131,34 @@ function testDown(trees: string[], row: number, column: number, sNum: string) {
   return true;
 }
 
+
+function getLeftScene(
+  trees: string[],
+  row: number,
+  col: number,
+  current: string
+) {
+  let left: number = col - 1;
+  let count: number = 0;
+  while (left >= 0) {
+    let next: string = trees[row][left];
+    if (next < current) {
+      count++;
+    }
+
+    if (next >= current) {
+      count++;
+      return count;
+    }
+
+    left--;
+  }
+
+  return count || 1;
+}
+
 function testLeft(trees: string[], column: number, s: string) {
-  let l = column - 1;
+  let l: number = column - 1;
   while (l >= 0) {
     if (s[l] >= s[column]) {
       return false;
@@ -63,6 +167,32 @@ function testLeft(trees: string[], column: number, s: string) {
   }
 
   return true;
+}
+
+
+function getRightScene(
+  trees: string[],
+  row: number,
+  col: number,
+  current: string
+) {
+  let right: number = col + 1;
+  let count: number = 0;
+  while (right < trees[0].length) {
+    let next: string = trees[row][right];
+    if (next < current) {
+      count++;
+    }
+
+    if (next >= current) {
+      count++;
+      return count;
+    }
+
+    right++;
+  }
+
+  return count || 1;
 }
 
 function testRight(trees: string[], column: number, s: string) {
@@ -77,4 +207,7 @@ function testRight(trees: string[], column: number, s: string) {
   return true;
 }
 
+
+
 console.log("Part One: ", getVisibleTrees(input));
+console.log("Part Two: ", getScenicScore(input));
