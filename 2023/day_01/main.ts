835e57f61accae0dd056d5c0675e52cb245858fb
davidkharbin <david_k_harbin@hotmail.com>
2023-12-22 13:05:25 -0700

    chore: completes part two in ts, removes cpp

--- 2023/day_01/main.ts
+++ 2023/day_01/main.ts
@@ -1,31 +1,72 @@
 const fs = require("node:fs");
+function main(): void {
+  fs.readFile("input.txt", "utf8", (err: any, data: string) => {
+    if (err) return;
 
-fs.readFile("input.txt", "utf8", (err: any, data: string) => {
-  if (err) return;
+    const dataStrings: string[] = data.split("\n");
 
-  const dataStrings: string[] = data.split("\n");
+    console.log();
+    console.log(partOne(dataStrings));
+  });
+}
+
+function partOne(lines: string[]): number {
   let sum: number = 0;
 
-  dataStrings.forEach((line) => {
+  const sMap = new Map();
+  sMap.set(1, "one");
+  sMap.set(2, "two");
+  sMap.set(3, "three");
+  sMap.set(4, "four");
+  sMap.set(5, "five");
+  sMap.set(6, "six");
+  sMap.set(7, "seven");
+  sMap.set(8, "eight");
+  sMap.set(9, "nine");
+
+  function checkMap(str: string, i: number) {
+    for (let k: number = 0; k <= sMap.size; k++) {
+      if (str.indexOf(sMap.get(k)) === i) {
+        return k;
+      }
+    }
+    return 0;
+  }
+
+  lines.forEach((line) => {
     let strNum: string = "";
 
     for (let i: number = 0; i < line.length; i++) {
-      if (Number(line[i]) >= 0) {
+      if (Number(line[i]) > 0) {
         strNum += line[i];
         break;
       }
-    }
-    
-    for (let j: number = line.length; j >= 0; j--) {
-      if (Number(line[j]) >= 0) {
-        strNum += line[j];
+
+      let mapNum = checkMap(line, i);
+      if (mapNum > 0) {
+        strNum += mapNum;
         break;
       }
     }
 
-    sum+=Number(strNum);
+    for (let j: number = line.length; j >= 0; j--) {
+      if (Number(line[j]) > 0) {
+        strNum += line[j];
+        break;
+      }
+
+      let mapNum = checkMap(line, j);
+      if (mapNum > 0) {
+        strNum += mapNum;
+        break;
+      }
+    }
+
+    let num = Number(strNum);
+    sum += num;
   });
 
-  console.log("SUM: ", sum);
   return sum;
-});
+}
+
+main();
