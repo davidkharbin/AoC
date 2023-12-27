f369254c22869746cb6038f01a8cb30e45882c22
davidkharbin <david_k_harbin@hotmail.com>
2023-12-27 10:19:07 -0700

    chore: structures input data into matrix

---
+++ 2023/day_03/main.ts
@@ -0,0 +1,11 @@
+const fs = require("node:fs");
+
+function main(): void {
+  fs.readFile("input.data", "utf8", (err: any, data: string) => {
+    if (err) return;
+
+    const matrix: string[] = data.split("\n");
+
+		
+  });
+}
