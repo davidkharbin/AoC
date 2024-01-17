499280d2346fa0cd21a3264aa10dca81b406060c
davidkharbin <david_k_harbin@hotmail.com>
2024-01-17 12:43:33 -0700

    build: removes `.js` file using `ts-node`

--- 2023/day_02/main.js
+++
@@ -1,9 +0,0 @@
-var fs = require("node:fs");
-function main() {
-    fs.readFile("input.txt", "utf8", function (err, data) {
-        if (err)
-            return;
-        var dataStrings = data.split("\n");
-        console.log('hi');
-    });
-}
