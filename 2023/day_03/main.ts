7db94e3ebe71f4e178de1d8ec8978a10162c8eb9
davidkharbin <david_k_harbin@hotmail.com>
2024-01-17 16:45:20 -0700

    chore: iteration and stubbed helper functions

--- 2023/day_03/main.ts
+++ 2023/day_03/main.ts
@@ -1,13 +1,40 @@
-const fs = require("node:fs");
+import fs from "node:fs";
+
+const specialCharacters: string = "@#$%&*-+=/";
+
+function isNumeric(char: string): boolean {
+  return !isNaN(Number(char));
+}
+
+function isValidPartNumber(y: number, x1: number, x2: number): boolean {
+
+}
 
 function main(): void {
   fs.readFile("input.data", "utf8", (err: any, data: string) => {
-    if (err) return;
-
     const matrix: string[] = data.split("\n");
+    const total: number = 0;
 
-		console.log(matrix)
-		
+    for (let row: number = 0; row < matrix.length; row++) {
+      for (let col: number = 0; col < matrix[row].length; col++) {
+        console.log(row, col);
+        if (isNumeric(matrix[row][col])) {
+          let stringNum: string = matrix[row][col];
+          let start: number = col;
+          let end: number = col++;
+
+          while (isNumeric(matrix[row][col])) {
+            stringNum += matrix[row][col];
+            end = col++;
+          }
+          console.log(stringNum, start, end);
+
+          if (isValidPartNumber(row, start, end)) {
+            total += Number(stringNum);
+          }
+        }
+      }
+    }
   });
 }
 
