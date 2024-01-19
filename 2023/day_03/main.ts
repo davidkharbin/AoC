6ac0d8941dbae9d0b1c0bfdd1ff62ea6d0191eb4
davidkharbin <david_k_harbin@hotmail.com>
2024-01-19 16:53:49 -0700

    chore: day_03 part 1 solved with TypeScript

--- 2023/day_03/main.ts
+++ 2023/day_03/main.ts
@@ -6,18 +6,24 @@ function isNumeric(char: string): boolean {
   return !isNaN(Number(char));
 }
 
-function isValidPartNumber(y: number, x1: number, x2: number): boolean {
-
+function isValidPartNumber(str: string): boolean {
+  for (let i: number = 0; i < str.length; i++) {
+    if (specialCharacters.includes(str[i])) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function main(): void {
   fs.readFile("input.data", "utf8", (err: any, data: string) => {
     const matrix: string[] = data.split("\n");
-    const total: number = 0;
+    let total: number = 0;
 
-    for (let row: number = 0; row < matrix.length; row++) {
-      for (let col: number = 0; col < matrix[row].length; col++) {
-        console.log(row, col);
+    // the first and last rows and the first and last elements have been added for padding
+    // (removing the requirement to check for undefined neighbors)
+    for (let row: number = 1; row < matrix.length; row++) {
+      for (let col: number = 1; col < matrix[row].length - 1; col++) {
         if (isNumeric(matrix[row][col])) {
           let stringNum: string = matrix[row][col];
           let start: number = col;
@@ -27,14 +33,21 @@ function main(): void {
             stringNum += matrix[row][col];
             end = col++;
           }
-          console.log(stringNum, start, end);
 
-          if (isValidPartNumber(row, start, end)) {
+          const charsAbove: string = matrix[row - 1].slice(start - 1, end + 2);
+          const charsBelow: string = matrix[row + 1].slice(start - 1, end + 2);
+          const rightChar: string = matrix[row][start - 1];
+          const leftChar: string = matrix[row][end + 1];
+          const neighbors: string =
+            leftChar + rightChar + charsAbove + charsBelow;
+
+          if (isValidPartNumber(neighbors)) {
             total += Number(stringNum);
           }
         }
       }
     }
+    console.log(total);
   });
 }
 
